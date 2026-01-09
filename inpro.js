e01ef80032f2842fe175b87c640bd45ab52ba2fd
todoom1 <aa13976383@outlook.com>
2026-01-09 15:25:40 +0800

    Add files via upload

---
+++ inpro.js
@@ -0,0 +1,213 @@
+document.getElementById("pg").addEventListener("click", function() {
+  window.location.href = "./tea1.html";
+});
+document.getElementById("pp").addEventListener("click", function() {
+  window.location.href = "./pptest.html";
+});
+document.getElementById("jdb").addEventListener("click", function() {
+  window.location.href = "./jdb.html";
+});
+document.getElementById("mg").addEventListener("click", function() {
+  window.location.href = "./mg.html";
+});
+document.getElementById("ap").addEventListener("click", function() {
+  window.location.href = "./ap.html";
+});
+document.getElementById("bbin").addEventListener("click", function() {
+  window.location.href = "./bbin.html";
+});
+document.getElementById("ps").addEventListener("click", function() {
+  window.location.href = "./ps.html";
+});
+document.getElementById("qp").addEventListener("click", function() {
+  window.location.href = "./zy.html";
+});
+document.getElementById("cg").addEventListener("click", function() {
+  window.location.href = "https://demo.cg11pro.com/index_m.php";
+});
+document.getElementById("r88").addEventListener("click", function() {
+  window.location.href = "https://demo.r88games.com/";
+});
+document.getElementById("cq9").addEventListener("click", function() {
+  window.location.href = "https://www.cq9gaming.com/";
+});
+document.getElementById("ka").addEventListener("click", function() {
+  window.location.href = "https://www.kaga88.com/";
+});
+document.getElementById("gr").addEventListener("click", function() {
+  window.location.href = "https://grdemoweb.richgaming.net/";
+});
+document.getElementById("bjl").addEventListener("click", function() {
+  window.location.href = "https://www.ab8888.games:8888/?loginType=2&amp;trial=true&amp;language=zh_CN";
+});
+document.getElementById("tymn").addEventListener("click", function() {
+  window.location.href = "https://test.f66b88sport.com/h5/index.html#/";
+});
+document.getElementById("cpmn").addEventListener("click", function() {
+  window.location.href = "https://pc.obcp.live/redirect.html";
+});
+document.getElementById("bzmj").addEventListener("click", function() {
+  window.location.href = "./bz";
+});
+document.getElementById("csby").addEventListener("click", function() {
+  window.location.href = "https://api.jdbgaming.com/game/demo?lang=zh-CN&amp;id=1";
+});
+document.getElementById("wlby").addEventListener("click", function() {
+  window.location.href = "https://api.jdbgaming.com/game/demo?lang=zh-CN&amp;id=16";
+});
+document.getElementById("jbyc").addEventListener("click", function() {
+  window.location.href = "https://multi-cdn.cg11pro.com/demo/JpFishing/?gameMode=1&amp;showHome=true&amp;language=en&amp;uiType=2&amp;coinSymbol=&amp;homeUrl=https%3A%2F%2Fdemo.cg11pro.com%2F&amp;token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiIwMDE0In0.Rau4Q1uBSLPXfO8rNLZhdmzxZS8LtVr0XfIqBQqfOR";
+});
+document.getElementById("xjby").addEventListener("click", function() {
+  window.location.href = "https://multi-cdn.cg11pro.com/demo/JpFishing/?gameMode=1&showHome=true&language=en&uiType=2&coinSymbol=&homeUrl=https%3A%2F%2Fdemo.cg11pro.com%2F&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiIwMDE0In0.Rau4Q1uBSLPXfO8rNLZhdmzxZS8LtVr0XfIqBQqfOR";
+});
+document.getElementById("hlby").addEventListener("click", function() {
+  window.location.href = "https://fiweb.cqgame.games/h5//AT05/?token=GUESTPASS&language=zh-cn&dollarsign=Y&app=N&detect=N&language=zh-cn";
+});
+document.getElementById("ypby").addEventListener("click", function() {
+  window.location.href = "https://fiweb.cqgame.games/h5//AT01/?token=GUESTPASS&language=zh-cn&dollarsign=Y&app=N&detect=N&language=zh-cn";
+});
+document.getElementById("hjyc").addEventListener("click", function() {
+  window.location.href = "https://fiweb.cqgame.games/h5//AB3/?token=guest&language=zh-cn";
+});
+document.getElementById("hjyc2").addEventListener("click", function() {
+  window.location.href = "https://fiweb.cqgame.games/h5//GO06/?language=zh-cn&token=guest";
+});
+document.getElementById("oy").addEventListener("click", function() {
+  window.location.href = "https://www.ouxyi.me/ul/YbU25D?channelId=40221549";
+});
+document.getElementById("ba").addEventListener("click", function() {
+  window.location.href = "https://www.maxweb.academy/referral/earn-together/refer-in-hotsummer/claim?hl=zh-CN&ref=GRO_20338_SI463&utm_source=default";
+});
+document.getElementById("xnb").addEventListener("click", function() {
+  window.location.href = "./xnb.html";
+});
+document.getElementById("ggy").addEventListener("click", function() {
+  window.location.href = "https://gouvpn.cn/index.php#/register?code=ZVPKqeOI";
+});
+document.getElementById("dkd").addEventListener("click", function() {
+  window.location.href = "https://www.dukadi.xyz/#/register?code=2Rk31VCp";
+});
+document.getElementById("dm").addEventListener("click", function() {
+  window.location.href = "https://cdn.damai222.shop/#/register?code=JGQT6iHz";
+});
+document.getElementById("jryw").addEventListener("click", function() {
+  window.location.href = "https://60s.lylme.com/";
+});
+document.getElementById("hwid").addEventListener("click", function() {
+  window.location.href = "https://id.ali-door.top/share/damai";
+});
+document.getElementById("rjdq").addEventListener("click", function() {
+  window.location.href = "https://52qfx.lanzouw.com/b01jdki3a";
+});
+document.getElementById("dxjm").addEventListener("click", function() {
+  window.location.href = "http://www.eomsg.com/appweb/signUp.html?inviter=1o0je3kd";
+});
+document.getElementById("htyw").addEventListener("click", function() {
+  window.location.href = "https://1000tool.com/text/penmanship/";
+});
+document.getElementById("jable").addEventListener("click", function() {
+  window.location.href = "https://jable.tv/";
+});
+document.getElementById("you").addEventListener("click", function() {
+  window.location.href = "https://www.youporn.com/";
+});
+document.getElementById("njav").addEventListener("click", function() {
+  window.location.href = "https://njav.tv/";
+});
+document.getElementById("phub").addEventListener("click", function() {
+  window.location.href = "https://pornhub.com";
+});
+document.getElementById("air").addEventListener("click", function() {
+  window.location.href = "https://tv-skyairav.cc/";
+});
+
+document.getElementById("12").innerHTML="PG模拟器";
+
+document.getElementById("13").innerHTML="PP模拟器";
+
+document.getElementById("14").innerHTML="JBD模拟器";
+
+document.getElementById("15").innerHTML="MG模拟器";
+
+document.getElementById("16").innerHTML="AP模拟器";
+
+document.getElementById("17").innerHTML="BBIN模拟器";
+
+document.getElementById("18").innerHTML="PS模拟器";
+
+document.getElementById("19").innerHTML="棋牌模拟器";
+
+document.getElementById("20").innerHTML="cg模拟器";
+
+document.getElementById("21").innerHTML="R88模拟器";
+
+document.getElementById("22").innerHTML="CQ9模拟器";
+
+document.getElementById("23").innerHTML="KA模拟器";
+
+document.getElementById("24").innerHTML="GR模拟器";
+
+document.getElementById("25").innerHTML="百家乐模拟器";
+
+document.getElementById("26").innerHTML="体育模拟器";
+
+document.getElementById("27").innerHTML="彩票模拟器";
+
+document.getElementById("28").innerHTML="爆庄秘籍";
+
+document.getElementById("29").innerHTML="财神捕鱼";
+
+document.getElementById("30").innerHTML="五龙捕鱼";
+
+document.getElementById("31").innerHTML="金爆鱼场";
+
+document.getElementById("32").innerHTML="星际捕鱼";
+
+document.getElementById("33").innerHTML="欢乐捕鱼";
+
+document.getElementById("34").innerHTML="一炮捕鱼";
+
+document.getElementById("35").innerHTML="皇金渔场";
+
+document.getElementById("36").innerHTML="皇金渔场2";
+
+document.getElementById("37").innerHTML="欧易";
+
+document.getElementById("51").innerHTML="币安";
+
+document.getElementById("38").innerHTML="虚拟币入门";
+
+document.getElementById("39").innerHTML="狗狗云vpn";
+
+document.getElementById("40").innerHTML="杜卡迪vpn";
+
+document.getElementById("41").innerHTML="大麦VPN";
+
+document.getElementById("42").innerHTML="今日要闻";
+
+document.getElementById("43").innerHTML="海外ID";
+
+document.getElementById("44").innerHTML="软件大全";
+
+document.getElementById("45").innerHTML="短信接码";
+
+document.getElementById("46").innerHTML="jable";
+
+document.getElementById("47").innerHTML="youporn";
+
+document.getElementById("48").innerHTML="njav";
+
+document.getElementById("49").innerHTML="pornhub";
+
+document.getElementById("50").innerHTML="Airav";
+
+document.getElementById("52").innerHTML="🔥PG模拟器大全";
+
+document.getElementById("53").innerHTML="捕鱼模拟器";
+
+document.getElementById("54").innerHTML="虚拟币";
+
+document.getElementById("55").innerHTML="其他分享&VPN-开VPN可进蓝名";
+
+document.getElementById("56").innerHTML="如遇本站功能无法正常使用，请点击下载注册狗狗云VPN，即可突破网络封锁正常使用";
---
+++ list.js
@@ -0,0 +1,45 @@
+document.getElementById("qps").addEventListener("click", function() {
+  window.location.href = "https://hywkj.top/?channel=77777711";
+});
+document.getElementById("ff").addEventListener("click", function() {
+  window.location.href = "https://lncqc.jgqvji.com/app/register.php?site_id=1050&topId=1551767873639698&selfPlanId=3790275";
+});
+document.getElementById("pyq").addEventListener("click", function() {
+  window.location.href = "http://yhfkj.top/?channel=77777711";
+});
+document.getElementById("ltf").addEventListener("click", function() {
+  window.location.href = "https://www.ksxkj.top?p=9310267&n=11350227&y=77777711&j=112424&a=1";
+});
+document.getElementById("g66").addEventListener("click", function() {
+  window.location.href = "https://04cd7.fystea.com/d2BraX3Hiw";
+});
+document.getElementById("zt").addEventListener("click", function() {
+  window.location.href = "https://pkajim.hfjcl.com/app/register.php?site_id=1031&topId=34033";
+});
+document.getElementById("t1").addEventListener("click", function() {
+  window.location.href = "https://lncqc.jgqvji.com/app/register.php?site_id=1051&topId=527419";
+});
+document.getElementById("st").addEventListener("click", function() {
+  window.location.href = "https://lejva.tprsmi.com/app/register.php?site_id=1058&topId=941703&selfPlanId=828739";
+});
+
+document.getElementById("0").innerHTML="🔥十年信誉平台推荐";
+
+document.getElementById("1").innerHTML="朋友圈";
+
+document.getElementById("2").innerHTML="棋牌室";
+
+document.getElementById("3").innerHTML="G66";
+
+document.getElementById("4").innerHTML="乐太坊";
+
+document.getElementById("5").innerHTML="征途国际";
+
+document.getElementById("6").innerHTML="非凡娱乐";
+
+document.getElementById("9").innerHTML="Top1体育";
+
+document.getElementById("11").innerHTML="胜天国际";
+
+document.getElementById("57").innerHTML="PG777.MY";
+
